93b42b70b11b76ce04640c2625724acd461d702a
OksanaKozachenko <110740697+OksanaKozachenko@users.noreply.github.com>
2022-08-20 17:05:42 +0200

    My message from remote

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first message");
         System.out.println("My second message");
-
+        System.out.println("My third message from remote");
     }
 }
